2a68bdd37cca78f1bf19fd9da640e1f201baff18
Ploy Primchayaporn <137378257+Primchayaporn@users.noreply.github.com>
2023-10-05 13:52:37 +0700

    hw9-6

--- Homework/HW09/hw09-6-merge-array.cpp
+++ Homework/HW09/hw09-6-merge-array.cpp
@@ -66,3 +66,74 @@
         Merge Array1 & Array2 to Array3
         Array3 = 9 8 8 7 6 6 5 3 3 3 2 2 1 1
 */
+#include <stdio.h>
+int main () {
+    int N01 , N02 , N03 , hold ;
+    int arr01[ 100 ] , arr02[ 100 ] , arr03 [ 100 ] ;
+    int i , j ;
+
+    printf ( "Enter element of Array1 :\n" ) ;
+    scanf ( "%d", &N01 ) ;
+
+    for (int i = 0; i < N01; i++) {
+        printf( "--| Array1 [%d] :\n",i ) ;
+        scanf( "%d", &arr01[ i ] ) ;
+    }
+
+    printf ( "Enter element of Array2 :\n" ) ;
+    scanf ( "%d", &N02 ) ;
+
+    for (int i = 0; i < N02; i++) {
+        printf( "--| Array2 [%d] :\n",i ) ;
+        scanf( "%d", &arr02[ i ] ) ;
+    }
+    
+    for (int i = 0; i < N01; i++) {
+    for (int j = i + 1; j < N01; j++) {
+        if (arr01[i] > arr01[j]) {
+            hold = arr01[j];
+            arr01[j] = arr01[i];
+            arr01[i] = hold;
+            }
+        }
+    }
+
+    for (int i = 0; i < N02; i++) {
+        for (int j = i + 1; j < N02; j++) {
+            if (arr02[ i ] > arr02[ j ] ) {
+                hold = arr02[ j ];
+                arr02[ j ] = arr02[ i ] ;
+                arr02[ i ] = hold;
+            }
+        }
+    }
+
+    N03 = N01 + N02 ;
+
+   for ( int i = 0 ; i < N01 ; i++ ) {
+    arr03[ i ] = arr01[ i ];
+    }
+
+    for ( int i = 0; i < N02; i++ ) {
+        arr03[i + N01] = arr02[ i ] ;
+    }
+    
+    
+    for (int i = 0; i < N03; i++) {
+        for (int j = i + 1 ; j < N03 ; j++) {
+            if ( arr03[ i ] < arr03[ j ]) {
+                hold = arr03[ i ] ;
+                arr03[ i ] = arr03[ j ] ;
+                arr03 [ j ] = hold ;
+            }
+        }
+    }
+    printf( "Merge Array1 & Array2 to Array3\n" ) ;
+    printf( "Array3 =") ;
+    for ( int i = 0; i < N03; i++ ) {
+        printf( " %d",arr03[ i ] ) ;
+    }
+    
+
+    return 0 ;
+}
